7a7f0f76a5f13effa41988885f0e061039429d16
Derek Roberts <derek.roberts@gmail.com>
2025-05-07 19:54:02 -0700

    Usage

--- scripts/user_add.sh
+++ scripts/user_add.sh
@@ -3,6 +3,13 @@
 set -euo pipefail
 set -x
 
+# Input validation
+if [ -z "${1:-}" ]; then
+    echo "Create a PR-appropriate user in the Crunchy DB"s
+    echo "Usage: $0 <github_pr_no> <repo_name>"
+    exit 1
+fi
+
 # Inputs and variables
 PR_NO="${1:-0}"
 REPO="${2:-action-crunchy}"
