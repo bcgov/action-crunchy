19abf35950994f86a53dbc88a851a83a5367f969
Chris Berg <chris.berg@ao-technologies.com>
2026-06-10 15:34:54 -0700

    fix:  Added refrence to branch updates

--- scripts/deploy_db.sh
+++ scripts/deploy_db.sh
@@ -71,8 +71,6 @@ fi
 if [ "${DEBUG_MODE:-false}" = "true" ]; then
   helm upgrade --debug --dry-run --install --wait "$RELEASE_NAME" --values ./values.yml ./$APP_NAME-$CHART_VERSION.tgz $SET_STRINGS
 else
-  echo "helm upgrade --debug --dry-run --install --wait "$RELEASE_NAME" --values ./values.yml ./$APP_NAME-$CHART_VERSION.tgz $SET_STRINGS"
-  helm upgrade --debug --dry-run --install --wait "$RELEASE_NAME" --values ./values.yml ./$APP_NAME-$CHART_VERSION.tgz $SET_STRINGS
   helm upgrade --install --wait "$RELEASE_NAME" --values ./values.yml ./$APP_NAME-$CHART_VERSION.tgz $SET_STRINGS
 fi
 # Verify successful db deployment; wait retry 10 times with 60 seconds interval
